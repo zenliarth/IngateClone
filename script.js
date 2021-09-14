46e5a0faaee5f2ecfe3d3f19ab2d94add7cb893f
zenliarth <54330804+zenliarth@users.noreply.github.com>
2021-09-14 01:04:31 -0300

    Added Light Mode

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const sections = document.querySelectorAll('.js-scroll');
 const div1anima = document.querySelector('.section1');
-/* const loader = document.querySelector('.loader'); */
+const loader = document.querySelector('.loader');
+const btnToggle = document.querySelector('.ativar');
+const toggle = document.querySelectorAll('.toggle');
+const darkMode = document.querySelector('.color');
+const imgDarkMode = document.getElementsByTagName('img')[1];
 
 function animaScroll() {
   sections.forEach((section) => {
@@ -13,13 +17,30 @@ function animaScroll() {
 function anima() {
   div1anima.classList.add('ativo');
 }
-/* function animaLoader() {
-  if (body !== true) {
-    loader.classList.add('ativo');
-  } else {
-    loader.classList.remove('ativo');
-  }
-} */
+function animaToggle() {
+  toggle.forEach((toggle) => {
+    toggle.classList.toggle('ativo');
+  });
+}
+function animaColor() {
+  darkMode.classList.toggle('ativo');
+  imgDarkMode.classList.toggle('ativo');
+}
 
+function trocaImg() {
+  if (imgDarkMode.classList.contains('ativo')) {
+    imgDarkMode.src = 'images/night2.png';
+    btnToggle.style.background = '#121214';
+  } else {
+    imgDarkMode.src = 'images/night1.png';
+    btnToggle.style.background = '#ff248a';
+  }
+}
 window.addEventListener('scroll', animaScroll);
 window.addEventListener('load', anima);
+window.addEventListener('load', function () {
+  loader.style.display = 'none';
+});
+btnToggle.addEventListener('click', animaToggle);
+btnToggle.addEventListener('click', animaColor);
+btnToggle.addEventListener('click', trocaImg);
